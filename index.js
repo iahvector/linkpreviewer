5430db5afb52ae2d8e63bdf183c099b7a972ab96
Islam Hassan <iah.vector@gmail.com>
2014-09-13 00:10:38 +0300

    enable CORS

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ var kraken = require('kraken-js'),
 
 app.use(kraken(options));
 
+// Enables CORS
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+ 
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+ 
+ 
+// enable CORS!
+app.use(enableCORS);
+
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
