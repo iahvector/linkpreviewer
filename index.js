1d57a0ff709a2621abfba4931ffad2bc8e6e9bcd
Jonathan Hindi <j@jonathanhindi.me>
2014-09-13 01:36:29 +0200

    remove unneeded domain from CORS

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(kraken(options));
 
 // Enables CORS
 var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'audibu.com, api.audibu.com, links.audibu.com');
+    res.header('Access-Control-Allow-Origin', 'audibu.com, api.audibu.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
  
